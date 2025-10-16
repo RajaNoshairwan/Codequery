2ae71a4b55598260a7f2583f6410436485fc525a
Raja Noshairwan <noshairwan.kamal03@gmail.com>
2025-10-16 10:41:50 -0700

    fix: import Offset from flutter for nudge()

--- lib/game/learning_game.dart
+++ lib/game/learning_game.dart
@@ -2,6 +2,7 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/events.dart';
 import 'player.dart';
+import 'package:flutter/material.dart' show Offset;
 import 'door.dart';
 import 'laptop.dart';
 
